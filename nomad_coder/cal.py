215bc60a784c5a2acd29a046ebfed8182ec1a160
Jay-Thomas-code <rubat0@naver.com>
2021-07-17 02:10:03 +0900

    def commit

--- nomad_coder/cal.py
+++ nomad_coder/cal.py
@@ -1,55 +1,54 @@
 #!/usr/bin/env python3v
 
-#calculator
-def calculator():
-    print("6가지 기능이 탑재된 계산기입니다.")
-
-#num1
 def num1(): 
-    num1 = input("첫 번째 숫자 : ")
-    if num1.isdigit():
-        int(num1)
-
+    v_num1 = input("첫 번째 숫자 : ")
+    
+    if type(v_num1) == str:
+        int(v_num1) 
+        print(type(v_num1))
     else :
         print("올바른 수를 입력하세요")  
         return calculator()
+num1()
 
 #num2
-def num2():
-    num2 = input("두 번째 숫자를 입력하세요 : ")
-    if num2.isdigit():
-        int(num2)
+def num2():    
+    global v_num2 
+    v_num2 = input("두 번째 숫자를 입력하세요 : ")
+    if v_num2.isdigit():
+        int(v_num2)
         return
         
     else :
         print("올바른 수를 입력하세요")  
         return calculator()
+num2()
 
-def opreator(v_num1 = num1,v_num2 = num2):
+def opreator(n_1 = v_num1,n_2 = v_num2):
+
     opreator = input("연산자를 입력하세요 : ")
     if opreator =="+":
-        return print(opreator(v_num1+v_num2))
+        return print(opreator(n_1+n_2))
 
-    elif opreator =="-":
-        return print(opreator(v_num1-v_num2))
+    if opreator =="-":
+        return print(opreator(n_1-n_2))
         
-    elif opreator =="*":
-        return print(opreator(v_num1*v_num2)) 
+    if opreator =="*":
+        return print(opreator(n_1*n_2)) 
         
-
-    elif opreator =="/":
-        return print(opreator(v_num1/v_num2))
+    if opreator =="/":
+        return print(opreator(n_1/n_2))
         
-    elif opreator =="//":
-        return print(opreator(v_num1//v_num2))
+    if opreator =="//":
+        return print(opreator(n_1//n_2))
         
+    if opreator =="%":
+        return print(opreator(n_1%n_2))
 
-    elif opreator =="%":
-        return print(opreator(v_num1%v_num2))
+#calculator
+def calculator():
+    print("6가지 기능이 탑재된 계산기입니다.")
 
 
 
-calculator()
-num1()
-opreator()
-num2()
+print(type(v_num1))
