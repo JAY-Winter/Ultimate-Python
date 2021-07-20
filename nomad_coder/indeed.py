64a2d7c17b639bce0c04e625002e1028304e1a8a
Jay-Thomas-code <rubat0@naver.com>
2021-07-20 09:52:26 +0900

    max_page()

--- nomad_coder/indeed.py
+++ nomad_coder/indeed.py
@@ -1,7 +1,8 @@
+#https://kr.indeed.com/jobs?q=python&limit=50&radius=25&start=0 크롤링
 import requests
 from bs4 import BeautifulSoup
 
-# 기본 구조
+# 페이지 기본 주소 
 
     # 1 https://kr.indeed.com/jobs?q=python&limit=50&radius=25&start=0
     # 2 https://kr.indeed.com/jobs?q=python&limit=50&radius=25&start=50
@@ -11,13 +12,14 @@ from bs4 import BeautifulSoup
     # 6 https://kr.indeed.com/jobs?q=python&limit=50&radius=25&start=250
     # 7 https://kr.indeed.com/jobs?q=python&limit=50&radius=25&start=300
     # 8 https://kr.indeed.com/jobs?q=python&limit=50&radius=25&start=350
+    # n https://kr.indeed.com/jobs?q=python&limit=50&radius=25&start=(n-1)*50
 
-
-
-def max_url():
+#url 기반으로 max_page 찾는 함수
+def max_page():
     n = 1
     i = 0
-
+    pages = []
+# 임시로 350 까지 지정
     while i <= 350:
         i = (n-1)*50    
         
@@ -26,10 +28,19 @@ def max_url():
         
         soup = BeautifulSoup(html.text, 'html.parser')
         current_page = soup.find("b",{'aria-label':{n}}).string
+        pages.append(int(current_page))
 
-        print(current_page)
         n = n+1
+        print(pages)
+# 코드 마지막 부분
 
+        # 페이지 중 가장 큰 값을 찾는 반복문 
+        # 일단 i =(n-1)*50 에서 가장 큰 n 값을 찾는 것이 우선
+        # maxVal = pages[0]
+        # for i in range(1,len(pages)):
+        #     if maxVal < pages[i]:
+        #         maxVal = pages[i]
 
+        # print(maxVal)
 
-max_url()
+max_page()
