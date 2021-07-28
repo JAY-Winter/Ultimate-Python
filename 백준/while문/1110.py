14ef93d93e5b8fe9dd166d70ac87d291cc19cb72
Jay-Thomas-code <rubat0@naver.com>
2021-07-28 19:46:56 +0900

    백준 while 마지막 문제풀이

--- 백준/while문/1110.py
+++ 백준/while문/1110.py
@@ -1,32 +1,10 @@
-# 26 
-
-# 2 + 6 = 8 
-# 68
-
-# 6 + 8 = 14
-# 84
-
-# 8 + 4 = 12
-# 42
-
-# 4 + 2 = 6
-
-# 26
-
-# a =(45%10)
-# print(a)
-
-# b = (45//10)
-# print(b)
-
-import sys
-
 N = int(input())
 X = N
 
+
 def cycle_plus(N,X):
-        
-    answer = 0
+    
+    answer = 0        
 
     while True :
         try : 
@@ -37,21 +15,14 @@ def cycle_plus(N,X):
             
             N = (units*10) + (tmp%10)
             answer += 1
-
+            print(N)
             if N == X : 
                 print(answer)
                 break
 
         except : 
             print("Occured Error")
+
 cycle_plus(N,X)
 
-# for i in range(5):
-#     tens = (N//10)
-#     units = (N%10)
-    
-#     tmp = tens + units
-    
-#     N = (units*10) + (tmp%10)
 
-#     print(N)
