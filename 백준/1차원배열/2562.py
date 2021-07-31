35bf7cefa33e9d15db1c73cc1e20dcb9b3667a14
Jay-Thomas-code <rubat0@naver.com>
2021-07-31 23:02:24 +0900

    백준문제풀이

--- 백준/1차원배열/2562.py
+++ 백준/1차원배열/2562.py
@@ -1,24 +1,27 @@
-import sys
 
-for i in range(9):
+def max():
 
-    list = list(map(int, sys.stdin.readline().split()))
-    print(list)
+    List = []
+    
+    for i in range(9):
 
-def max(list):
+        num = int(input())
+        List.append(num)
 
+    max = List[0]
     point = 0
-    max = list[0]
 
-    for i in range(len(list)-1) :
+    for i in range(len(List)-1) :
 
-        if max < list[i+1] :
-            max = list[i+1]
+        if max < List[i+1] :
+            max = List[i+1]
             point = i+2
             # index 상으로는 i+1 이 맞는데 우리는 몇 번째 위치하는지를 뽑아내야하므로
             # point = i+2 이다
-            print(max)
-            print( point)
+    print(max)
+    print(point)
         
 
-max(list)
+max()
+
+
---
+++ 백준/1차원배열/2577.py
@@ -0,0 +1,17 @@
+
+A = int(input())
+B = int(input())
+C = int(input())
+
+N = (A*B*C)
+
+def N_count(N):
+
+    
+
+    
+
+
+
+
+N_count(N)
