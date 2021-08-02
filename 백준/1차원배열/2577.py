a6c94872f9d3f28faf6509e4336576cdbeac53ae
Jay-Thomas-code <rubat0@naver.com>
2021-08-02 23:23:27 +0900

    문제풀이

--- 백준/1차원배열/2577.py
+++ 백준/1차원배열/2577.py
@@ -3,14 +3,23 @@ A = int(input())
 B = int(input())
 C = int(input())
 
-N = (A*B*C)
+N = str(A*B*C)
 
 def N_count(N):
 
+    list = ['0','1','2','3','4','5','6','7','8','9']
+    for i in range(len(list)):
 
+        point = list[i]
+        answer = 0
 
+        for j in range(len(list)-1):
 
+            if point == N[j+1] :
+                answer += 1
 
+        print(answer)
 
 
 N_count(N)
+
