29e39d376e6f3f1ca60a669d2e7caf89eb678ec5
= <1dontwannafreejazz@gmail.com>
2021-08-15 22:09:46 +0900

    commit

--- Etoos/main.py
+++ Etoos/main.py
@@ -1,27 +1,34 @@
 # from selenium import Keys
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
 
 URL = 'https://www.etoos.com/member/login.asp'
 
 driver = webdriver.Chrome('chromedriver')
 
 ID = "ilsan247"
-
+PW = "isd151208^^"
 
 driver.get(URL)
 
 # try:
-# id_box = driver.find_element_by_name("mem_id")
-id_box = driver.find_element_by_name("mem_id")
-print("정상 작동")
-id_box.send_keys("123", Keys.ENTER)
-# id_box.send_keys(ID)
 
-# id.send_keys("ilsan247")
+
+# id_box = driver.find_element_by_name("mem_id")
+
+
+
+# driver.find_element_by_name("mem_id").click()
+# ActionChains(driver).send_keys(ID).perform()
+
+driver.find_element_by_name("pwdtmp").click()
+ActionChains(driver).send_keys(PW).perform()
+
+print("정상 작동")
 
 driver.implicitly_wait(2)
-driver.close()
+# driver.close()
     
 # except :
 #     # print("오류 발생")
