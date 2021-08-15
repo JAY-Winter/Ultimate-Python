89dbba6dedd20c1668606816b3fcf549e0748c51
= <1dontwannafreejazz@gmail.com>
2021-08-15 15:09:51 +0900

    selenium

--- Etoos/main.py
+++ Etoos/main.py
@@ -1,20 +1,33 @@
-import requests
-from bs4 import BeautifulSoup
+# from selenium import Keys
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
-session = requests.session()
+URL = 'https://www.etoos.com/member/login.asp'
 
-url = "https://www.etoos.com/member/login.asp?returnUrl=http://ilsandonggu247.etoos.com/lms/index.do"
+driver = webdriver.Chrome('chromedriver')
 
-data = {
-    "mem_id" : "ilsan247",
-    "pwdtmp" : "isd151208^^"
+ID = "ilsan247"
+# PW = "isd151208^^"
 
-}
+driver.get(URL)
+
+# try:
+# id_box = driver.find_element_by_name("mem_id")
+id_box = driver.find_element_by_name("mem_id")
+print("정상 작동")
+id_box.send_keys("123", Keys.ENTER)
+# id_box.send_keys(ID)
+
+# id.send_keys("ilsan247")
+
+driver.implicitly_wait(2)
+driver.close()
+    
+# except :
+#     # print("오류 발생")
+#     driver.close()
 
 
-response = session.get(url)
-
-print(response.status_code)
 
 
 
