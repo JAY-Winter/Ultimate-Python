e820c15b02879d7f51720477b9bd09b8c71306c3
= <1dontwannafreejazz@gmail.com>
2021-10-20 19:00:21 +0900

    디렉토리 정리

--- nomad_coder/def.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3v
-
-def greeting():
-    print('Hi!')
-
-greeting()
--- syntax/string2.py
+++
@@ -1,9 +0,0 @@
-a = 'Hello Python'
-print(a)
-#length
-print(len(a))
-
-
-#idnex
-print(a[0])
-print(a[1])
