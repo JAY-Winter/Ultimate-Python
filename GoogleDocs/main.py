6f59bdd7d1baf9057e953f1603aac36b64496055
= <rubat0@naver.com>
2021-10-23 23:41:07 +0900

    insertPageBrake, insertInlineImage 삽입 위치 수정

--- GoogleDocs/main.py
+++ GoogleDocs/main.py
@@ -1,6 +1,7 @@
 import os.path
 from googleapiclient.discovery import build
 from google.oauth2.credentials import Credentials
+import natsort
 
 SCOPES = [    
     "https://www.googleapis.com/auth/documents",
@@ -25,6 +26,9 @@ def main() :
     subject_file_list = os.listdir(subject_path)
     subject_file_list.remove(".DS_Store")
     subject_file_list.sort()
+    sorted_subject_file_list = natsort.natsorted(subject_file_list)
+    
+    print(sorted_subject_file_list)
     
     print("데일리 테스트 시험지 작성 시작")
 
@@ -32,73 +36,69 @@ def main() :
     point = 0 
 
     while 1 <= function_lotation < 8 :
-        
+        # 새로운 페이지가 생성되면 그 페이지의 첫 번째로 가야함
+        # for i 문에 index_location = 1 을 선언했을 때
+        # 첫 번째 페이지의 첫 번째 index 로 삽입됨
         index_location = 1
 
         for i in range( len(subject) ) :
         
             n = 1
             
-            for j in range( point, len(subject_file_list) ) :
+            for j in range( point, len(sorted_subject_file_list) ) :
                 
-                subject_name_in_file_list = subject_file_list[j][0:3]
-                print(subject_name_in_file_list)
+                subject_name_in_file_list = sorted_subject_file_list[j][0:3]
+
                 if subject[i] == subject_name_in_file_list :
 
-                    urls = f'https://etoos-dailytest-storage.s3.ap-northeast-2.amazonaws.com/수학/{input_day}/{subject_file_list[j]}'
+                    urls = f'https://etoos-dailytest-storage.s3.ap-northeast-2.amazonaws.com/수학/{input_day}/{sorted_subject_file_list[j]}'
 
                     requests = [{
-
                         'insertInlineImage': {
-
                             'location': {'index': index_location},
                             'uri': urls ,
-
                         }
                     }]
 
                     body = {'requests': requests}
 
-                    response = service.documents().batchUpdate(
-                        documentId=document_id, body=body).execute()
+                    response = service.documents().batchUpdate(documentId=document_id, body=body).execute()
 
-                    insert_inline_image_response = response.get('replies')[0].get(
-                        'insertInlineImage')
+                    insert_inline_image_response = response.get('replies')[0].get('insertInlineImage')
 
                     print(f"""
-                    구글 docs 데일리테스트 {subject_file_list[j]}번 삽입 완료
+                    구글 docs 데일리테스트 {sorted_subject_file_list[j]}번 삽입 완료
                     """)
 
                     n += 1
                     point += 1
                     index_location += 1
 
-                elif subject[i] != subject_name_in_file_list :
+                else :
                 # 과목 명이 다를 때 새로운 페이지 생성
                     print("새로운 페이지 생성") 
 
                     requests = [{
-
                         'insertPageBreak' : {
-                            
                             'endOfSegmentLocation' : {}
                         }
                         }]
 
                     body = {'requests': requests}
 
-                    response = service.documents().batchUpdate(
-                    documentId=document_id, body=body).execute()
+                    response = service.documents().batchUpdate(documentId=document_id, body=body).execute()
 
-                    insert_page_break_response = response.get('replies')[0].get(
-                    'insertPageBreak')
+                    insert_page_break_response = response.get('replies')[0].get('insertPageBreak')
 
                     break
-
 
             # 반복문 i 끝났을 때
             function_lotation += 1
 
+    return print("""
+프로그램을 종료합니다.
+문의 : ETOOS247 일산동구점 정재현
+""")
 
 
 
